b8851b99fd86841bb456a35b3941c9ce63a9ebcb
jinu2300 <49512703+jinu2300@users.noreply.github.com>
2019-05-23 11:44:03 +0900

    upload index 파일

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		Select image to upload :
+		<input type="file" name="fileUp" id="fileUp">
+		<input type="submit" value="Upload" name="submit">
+	</form>
+</body>
+</html>
